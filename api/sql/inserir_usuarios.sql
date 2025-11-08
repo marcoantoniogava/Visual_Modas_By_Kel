c82f9800c162df7cdd06de75e1547c4b4c64ee0d
Marco Antônio de Freitas Gava <marcogava192425@gmail.com>
2025-11-08 16:13:16 -0300

    Cadastro de usuários, produtos e pedidos.

---
+++ api/sql/inserir_usuarios.sql
@@ -0,0 +1,22 @@
+USE visualmodasbykel;
+
+INSERT INTO usuarios (
+    nome,
+    sobrenome,
+    email,
+    senha,
+    telefone,
+    cpf,
+    role
+) VALUES
+    ('João', 'Silva', 'joao@gmail.com', '$2a$10$tuGD7wBFEnSwE8O6JZdsMe9ll0L0XD.IHeGHugpu/7zuwXO/ebFUC', '11987345621', '32165498701', 'user'),
+    ('Maria', 'Oliveira', 'maria@gmail.com', '$2a$10$tuGD7wBFEnSwE8O6JZdsMe9ll0L0XD.IHeGHugpu/7zuwXO/ebFUC', '11993421578', '87412536940', 'user'),
+    ('Pedro', 'Souza', 'pedro@gmail.com', '$2a$10$tuGD7wBFEnSwE8O6JZdsMe9ll0L0XD.IHeGHugpu/7zuwXO/ebFUC', '21981234576', '50678412399', 'user'),
+    ('Ana', 'Costa', 'ana@gmail.com', '$2a$10$tuGD7wBFEnSwE8O6JZdsMe9ll0L0XD.IHeGHugpu/7zuwXO/ebFUC', '31999874512', '19234785660', 'user'),
+    ('Lucas', 'Almeida', 'lucas@gmail.com', '$2a$10$tuGD7wBFEnSwE8O6JZdsMe9ll0L0XD.IHeGHugpu/7zuwXO/ebFUC', '41997863412', '74821956302', 'user'),
+    ('Carla', 'Ferreira', 'carla@gmail.com', '$2a$10$tuGD7wBFEnSwE8O6JZdsMe9ll0L0XD.IHeGHugpu/7zuwXO/ebFUC', '51998123764', '03867124598', 'user'),
+    ('Rafa', 'Ribeiro', 'rafa@gmail.com', '$2a$10$tuGD7wBFEnSwE8O6JZdsMe9ll0L0XD.IHeGHugpu/7zuwXO/ebFUC', '62996587412', '61784523901', 'user'),
+    ('Lia', 'Mendes', 'lia@gmail.com', '$2a$10$tuGD7wBFEnSwE8O6JZdsMe9ll0L0XD.IHeGHugpu/7zuwXO/ebFUC', '71991236845', '28491563780', 'user'),
+    ('Mateus', 'Barros', 'mateus@gmail.com', '$2a$10$tuGD7wBFEnSwE8O6JZdsMe9ll0L0XD.IHeGHugpu/7zuwXO/ebFUC', '81997654321', '90518247366', 'user'),
+    ('Sofia', 'Carvalho', 'sofia@gmail.com', '$2a$10$tuGD7wBFEnSwE8O6JZdsMe9ll0L0XD.IHeGHugpu/7zuwXO/ebFUC', '88991028734', '45039816274', 'user');
+
